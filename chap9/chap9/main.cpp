a11abb83389d151b117d28a3da302a1058b1746c
Eunjin Oh <ibsuny3214@naver.com>
2023-12-09 12:18:49 +0900

    make ramdom_factal

--- chap9/chap9/main.cpp
+++ chap9/chap9/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
 
 using namespace std;
 
 void super_write_vertical(int);
+void random_frantal(int,int,int,int);
+void display(int);
 
 int main() {
 	int n = 0;
 
 	
-		cout << "input num: ";
-		cin >> n;
-		super_write_vertical(n);
-	
+	random_frantal(-10,10,10,1);
+	cout << endl;
+	Sleep(1000);
+	random_frantal(-10, 10, 10, 1);
+	cout << endl;
+	Sleep(1000);
+	random_frantal(-10, 10, 10, 1);
+	cout << endl;
+	Sleep(1000);
+	random_frantal(-10, 10, 10, 1);
+	cout << endl;
+	Sleep(1000);
+	random_frantal(-10, 10, 10, 1);
 
 	return 0;
 }
@@ -30,5 +45,41 @@ void super_write_vertical(int n) {
 		super_write_vertical(n / 10);
 		cout << n % 10 << endl;
 		return;
+	}
+}
+
+void random_frantal(int left_hight, int right_hight, int width, int epslion) {
+	double mid_hight = 0;
+	double rand_num = (rand() % (left_hight * 2)) - left_hight;
+
+	assert(width > 0);
+	assert(epslion > 0);
+
+	srand((unsigned int)time(NULL));
+
+	if (width <= epslion)
+		display(right_hight);
+	else {
+		mid_hight = (left_hight + right_hight) / 2;
+		mid_hight += rand_num;
+		random_frantal(right_hight, mid_hight, width / 2, epslion);
+		random_frantal(mid_hight, left_hight, width / 2, epslion);
+	}
+}
+
+void display(int n) {
+	if (n > 0) {
+		cout << "           |";
+		for (int i = 0; i < n; i++)
+			cout << "*";
+		cout << endl;
+	}
+	else if (n < 0) {
+		for (int i = 0; i < 11 + n; i++)
+			cout << " ";
+		for (int j = 0; j < abs(n); j++)
+			cout << "*";
+
+		cout << "|"<<endl;
 	}
 }
